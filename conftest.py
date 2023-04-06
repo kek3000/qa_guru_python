06d5bb389412d376412f9b144977652130b4564b
kek3000 <b9@ya.ru>
2023-04-06 10:32:40 +0300

    homework1 - исправления

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selene.support.shared import browser
 
 # все общие фикстуры мы пишем в файле conftest.py, они будут видны всем тестовым классам по умолчанию
 
-@pytest.fixture(scope="function")  # scope="session" браузер не будет закрываться между тестами
+@pytest.fixture(scope="function")
 def browser_start():
     browser.config.window_width = 1024
     browser.config.window_height = 768
